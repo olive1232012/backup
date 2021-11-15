cfb714010c84a9d368b14ae4ea2f9cc8d90c4300
olive1232012 <olive1232012@live.com>
2021-11-15 12:48:09 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #Main backup script
-sudo sh /home/ubuntu/backup/bkup_rpimage/bkup_rpimage.sh start -c /mnt/nas_share/backups/*INSERT HOST/images/*INSERT HOST*$(date +%Y-%m-%d).img
+sudo sh /home/ubuntu/backup/bkup_rpimage/bkup_rpimage.sh start -c /mnt/nas_share/backups/*INSERT HOST*/images/*INSERT HOST*$(date +%Y-%m-%d).img
 
 # This shrinks the .img file to a smaller size so it can be applied to smaller SD Cards if needed to.
 sudo pishrink.sh -z  /mnt/nas_share/backups/*INSERT HOST*/images/*INSERT HOST*_$(date +%Y-%m-%d).img
